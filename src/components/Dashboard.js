f154375c878ba5b81d526625d7746f72812fd781
rajat chhetri <52805173+rajat19-al@users.noreply.github.com>
2021-07-23 11:15:36 -0400

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+          <h1 className= "mr-1">Welcome to Dashboard</h1>
+        
+      </div>
+    )
+  }
+}
+
